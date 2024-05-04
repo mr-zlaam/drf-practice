debe3168f6c6d6c645ff6e25ddd32e0a7d6a3957
Zlaam <mrzalaam@gmail.com>
2024-05-04 16:46:51 +0500

    update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from posts.views import (
-    homepage,
     list_posts,
     get_single_post,
     update,
@@ -10,9 +9,7 @@ from posts.views import (
 
 
 urlpatterns = [
-    path("create_post/", homepage, name="posts_home"),
-    path("create_and_get_post/", Create_and_Get_Posts.as_view(), name="p/get"),
-    path("posts_list/", list_posts, name="list_posts"),
+    path("create_and_get_post/", Create_and_Get_Posts.as_view(), name="post_&_get"),
     path("posts_list/<int:post_id>", get_single_post, name="get_single_post"),
     path("update_post/<int:post_id>/", update, name="update_post"),
     path("delete_post/<int:post_id>/", delete, name="delete_post"),
