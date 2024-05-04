207a753cf864def97c3ab03b3d0323265bc9b8ad
Zlaam <mrzalaam@gmail.com>
2024-05-04 17:29:49 +0500

    update views.py

--- posts/views.py
+++ posts/views.py
@@ -33,37 +33,71 @@ class Create_and_Get_Posts_View(APIView):
     def post(self, request: Request, *args, **kwargs):
         data_from_client = request.data
         try:
-            serialize_the_data = self.serializer_class(data=data_from_client)
-            if serialize_the_data.is_valid():
-                serialize_the_data.save()
-                response_from_serializers = {
-                    "message": "post created successfully",
-                    "success": True,
-                    "status": status.HTTP_201_CREATED,
-                    "data": serialize_the_data.data,
+            if (
+                data_from_client.get("title")
+                and data_from_client.get("content") is not None
+            ):
+                serialize_the_data = self.serializer_class(data=data_from_client)
+                if serialize_the_data.is_valid():
+                    serialize_the_data.save()
+                    response_from_serializers = {
+                        "message": "post created successfully",
+                        "success": True,
+                        "status": status.HTTP_201_CREATED,
+                        "data": serialize_the_data.data,
+                    }
+                    return Response(
+                        data=response_from_serializers, status=status.HTTP_201_CREATED
+                    )
+                error_from_serializer = {
+                    "message": "some thing went wrong while creating posts",
+                    "success": False,
+                    "status": status.HTTP_400_BAD_REQUEST,
+                    "data": serialize_the_data.error,
                 }
                 return Response(
-                    data=response_from_serializers, status=status.HTTP_201_CREATED
+                    data=error_from_serializer, status=status.HTTP_400_BAD_REQUEST
                 )
-            error_from_serializer = {
-                "message": "some thing went wrong while creating posts",
-                "success": False,
-                "status": status.HTTP_400_BAD_REQUEST,
-                "data": serialize_the_data.error,
-            }
-            return Response(
-                data=error_from_serializer, status=status.HTTP_400_BAD_REQUEST
-            )
-
+            else:
+                error = {
+                    "message": "title and content are required",
+                    "success": False,
+                    "status": status.HTTP_403_FORBIDDEN,
+                    "data": None,
+                }
+                return Response(data=error, status=status.HTTP_403_FORBIDDEN)
         except Exception as e:
-            raise APIException("some thing went wrong while creating post") from e
+            raise APIException("some thing went wrong while creating posts") from e
 
 
 # TODO: write this later
 
 
-class Retrieve_Update_Delete_View:
-    pass
+class Retrieve_Update_Delete_View(APIView):
+    serializer_class = PostSerializer
+
+    # Get Single Post
+    def get_single_post(
+        self,
+        request: Request,
+        post_id: int,
+    ):
+        try:
+            query_single_post = get_object_or_404(Post, pk=post_id)
+            serialize_the_single_post = self.serializer_class(
+                instance=query_single_post
+            )
+            response_after_serialization = {
+                "message": "OK",
+                "success": True,
+                "status": status.HTTP_200_OK,
+                "data": serialize_the_single_post.data,
+            }
+            return Response(
+                data=response_after_serialization, status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            raise APIException("Internal server error occurred!") from e
 
 
 @api_view(http_method_names=["GET"])
