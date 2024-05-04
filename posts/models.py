c0d026ebfaa21e19c0a723db1a2648ab09f651fd
Zlaam <mrzalaam@gmail.com>
2024-05-04 12:42:50 +0500

    update settings.py and models.py

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,9 @@ POST Class
 """
 
 
-class POST(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField(max_length=500)
+    content = models.TextField()
     createdAT = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
