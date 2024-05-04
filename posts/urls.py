822934e179c12323a0d726814628377ea70fed10
Zlaam <mrzalaam@gmail.com>
2024-05-04 15:26:55 +0500

    update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from posts.views import homepage, list_posts, get_single_post, update, delete
 urlpatterns = [
     path("create_post/", homepage, name="posts_home"),
     path("posts_list/", list_posts, name="list_posts"),
-    path("get_single_post/<int:post_id>", get_single_post, name="get_single_post"),
+    path("posts_list/<int:post_id>", get_single_post, name="get_single_post"),
     path("update_post/<int:post_id>/", update, name="update_post"),
     path("delete_post/<int:post_id>/", delete, name="delete_post"),
 ]
