ab3152073805fef42dc8d33e7dbcdf4b8e3bda03
Zlaam <mrzalaam@gmail.com>
2024-05-04 12:16:18 +0500

    update urls.py and views.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from posts.views import homepage, list_posts
+from posts.views import homepage, list_posts, post_detail
 
 
 urlpatterns = [
     path("home/", homepage, name="posts_home"),
     path("posts_list/", list_posts, name="list_posts"),
+    path("<int:index>", post_detail, name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,6 +6,20 @@ from django.http import HttpResponse
 
 
 # Create your views here.;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+posts = [
+    {"id": 1, "name": "John Doe", "age": 30},
+    {"id": 2, "name": "Jane Smith", "age": 25},
+    {"id": 3, "name": "Bob Johnson", "age": 40},
+    {"id": 4, "name": "Alice Brown", "age": 28},
+    {"id": 5, "name": "Mike Davis", "age": 35},
+    {"id": 6, "name": "Emily Taylor", "age": 22},
+    {"id": 7, "name": "Sarah Lee", "age": 32},
+    {"id": 8, "name": "Kevin White", "age": 45},
+    {"id": 9, "name": "Lisa Hall", "age": 38},
+    {"id": 10, "name": "Tom Harris", "age": 42},
+]
+
+
 @api_view(http_method_names=["POST", "GET"])
 def homepage(request: Request):
     if request.method == "POST":
@@ -22,16 +36,14 @@ def homepage(request: Request):
 
 @api_view(http_method_names=["GET"])
 def list_posts(request: Request):
-    posts = [
-        {"id": 1, "name": "John Doe", "age": 30},
-        {"id": 2, "name": "Jane Smith", "age": 25},
-        {"id": 3, "name": "Bob Johnson", "age": 40},
-        {"id": 4, "name": "Alice Brown", "age": 28},
-        {"id": 5, "name": "Mike Davis", "age": 35},
-        {"id": 6, "name": "Emily Taylor", "age": 22},
-        {"id": 7, "name": "Sarah Lee", "age": 32},
-        {"id": 8, "name": "Kevin White", "age": 45},
-        {"id": 9, "name": "Lisa Hall", "age": 38},
-        {"id": 10, "name": "Tom Harris", "age": 42},
-    ]
     return Response(data=posts, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=["GET"])
+def post_detail(request: Request, index: int):
+    if index > len(posts):
+        return Response(
+            data={"message": "Post not found"}, status=status.HTTP_404_NOT_FOUND
+        )
+    post = posts[index]
+    return Response(data=post, status=status.HTTP_200_OK)
