b655ed9c8a70c634c7905b6744b9e5573c74fec2
Zlaam <mrzalaam@gmail.com>
2024-05-04 12:22:35 +0500

    create 1 file and update 1 file

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+""" 
+POST Class
+"""
+
+
+class POST(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500)
+    createdAT = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
