c2c2419feb4163568746caac99179c5a7fd1a7f4
Zlaam <mrzalaam@gmail.com>
2024-05-04 15:02:20 +0500

    update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from posts.views import homepage, list_posts, post_detail
+from posts.views import homepage, list_posts, get_single_post, update, delete
 
 
 urlpatterns = [
     path("home/", homepage, name="posts_home"),
     path("posts_list/", list_posts, name="list_posts"),
-    path("<int:post_id>", post_detail, name="post_detail"),
+    path("get_single_post/<int:post_id>", get_single_post, name="get_single_post"),
+    path("update/<int:post_id>/", update, name="update_post"),
+    path("delete/<int:post_id>/", delete, name="delete_post"),
 ]
