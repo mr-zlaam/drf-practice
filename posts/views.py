7be0f60d02157a4e027d22eb3b3409643aa8e939
Zlaam <mrzalaam@gmail.com>
2024-05-05 18:23:44 +0500

    create 1 file and update 1 file

--- posts/views.py
+++ posts/views.py
@@ -1,50 +1,20 @@
-from rest_framework.request import Request
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import status, generics, mixins
-from rest_framework.decorators import APIView
-from rest_framework.exceptions import APIException
-from django.http import HttpResponse
+from rest_framework import viewsets, status
+from rest_framework.request import Request
 from posts.models import Post
 from posts.serializers import PostSerializer
-from django.shortcuts import get_object_or_404
+
+# create your view set
 
 
-# GET & POST method to get and post data.
-class Create_and_Get_Posts_View(
-    generics.GenericAPIView,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-):
-    serializer_class = PostSerializer
-    queryset = Post.objects.all()
+class PostViewSet(viewsets.ViewSet):
+    def list(self, request: Request):
+        queryset = Post.objects.all()
+        serializers = PostSerializer(instance=queryset, many=True)
+        return Response(data=serializers.data, status=status.HTTP_200_OK)
 
-    # List all the posts
-    def get(self, request: Request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    # Create the post
-    def post(self, request: Request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-# TODO: write this later
-
-
-class Retrieve_Update_Delete_View(
-    generics.GenericAPIView,
-    mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.DestroyModelMixin,
-):
-    serializer_class = PostSerializer
-    queryset = Post.objects.all()
-
-    # Get Single Post
-    def get(self, request: Request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request: Request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request: Request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    def retrieve(self, request: Request, pk: int):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(instance=post)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
