2989643b2ddf6162c7430d6d99debcc3c5c09148
Zlaam <mrzalaam@gmail.com>
2024-05-05 19:23:11 +0500

    update PythonImportHelper-v2-Completion.json and models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Super user has to have is_staff being true")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Super user has to have is_superuser being true")
+        return self.create_user(email=email, password=password)
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
